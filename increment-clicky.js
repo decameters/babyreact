eb3fb2ee544baf765a9c1abe1ab20743edb6ee93
Elizabeth.Elton2 <eeeqqqeee@gmail.com>
2018-11-26 13:46:57 -0600

    tutorials

--- increment-clicky.js
+++ increment-clicky.js
@@ -35,7 +35,7 @@ class Button extends React.Component {
         return (
           <div>
             <Button incrementValue={1} onClickFunction={this.incrementCounter}/>
-          <Button incrementValue={5} onClickFunction={this.incrementCounter}/>
+            <Button incrementValue={5} onClickFunction={this.incrementCounter}/>
             <Button incrementValue={10} onClickFunction={this.incrementCounter}/>
             <Button incrementValue={100} onClickFunction={this.incrementCounter}/>
           <Result counter={this.state.counter}/>
